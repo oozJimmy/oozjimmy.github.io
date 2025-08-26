63878ae14fa33f279c03b162b7f623ac0f98a123
oozjimmy <jkinsley849@gmail.com>
2025-08-25 20:09:38 -0400

    State before jekyll integration

---
+++ dev/serv.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+)
+
+func main(){
+	port := 3000
+
+	http.Handle("/", http.FileServer(http.Dir("../")))
+	http.Handle("/img", http.FileServer(http.Dir("../static/img")))
+
+	fmt.Printf("Starting server listening to port %d\n", port)
+
+	//http.ListenAndServe(fmt.Sprintf(":%d",port),nil)
+	err := http.ListenAndServe(":3000",nil)
+	fmt.Println("Error:",err)
+}
